3f483e96c855bb134425aa022ace1330876b67ca
Brandon El Haj <brandon.elhaj@gmail.com>
2019-01-29 21:38:12 -0800

    Fifth commit

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
-function createGreeting(name, age) {
-    const yearOfBirth = 2019 - 33;
-    return `"Hi my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}"`;
+function yearOfBirth(age) {
+    return 2019 - age;
+}
 
+function createGreeting(name, age) {
+    const yob = yearOfBirth(age);
+    return `"Hi my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}"`;
 }
 
 const greeting1 = createGreeting();
 console.log(greeting1);
+
