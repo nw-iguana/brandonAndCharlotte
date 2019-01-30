f59ed933fb729548954ca8fe21972cfc3e6f425e
Brandon El Haj <brandon.elhaj@gmail.com>
2019-01-29 21:30:58 -0800

    Fifth Push

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 function createGreeting(name, age) {
-    return `"Hi my name is ${name} and I'm ${age}."`;
+    const yearOfBirth = 2019 - 33;
+    return `"Hi my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}"`;
+
 }
 
 const greeting1 = createGreeting();
