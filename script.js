6fd604174c423a755db39cb35ed5d9c8b5534839
Brandon El Haj <brandon.elhaj@gmail.com>
2019-01-30 18:34:11 -0800

    final commit

--- script.js
+++ script.js
@@ -12,18 +12,20 @@ function createGreeting(name, age) {
     if (name === undefined || age === undefined) {
         throw new Error("Arguments not valid.");
     }
-    else if (isNaN(age) || typeof name !== "string") {
-        throw new TypeError();
+    else if (isNaN(age)) {
+        throw new TypeError("Age must be a number");
+    }
+    else if (typeof name !== "string") {
+        throw new TypeError("Name must be a string");
     }
     const yob = yearOfBirth(age);
     return `"Hi my name is ${name} and I'm ${age}. I was born in ${yearOfBirth}"`;
-
 }
 
 try {
-    const greeting1 = createGreeting(Brandon, -6);
+    const greeting1 = createGreeting("Brandon", -6);
     console.log(greeting1);
-    const greeting2 = createGreeting(Brandon, 33);
+    const greeting2 = createGreeting("Brandon", 33);
     console.log(greeting2);
 }
 catch(e) {
