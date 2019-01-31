11b3ae293ce5dc3e70550988ef7673df899e1f94
Charlotte Shih <shih.charlotte@gmail.com>
2019-01-30 20:07:12 -0800

    drill 2 second commit

--- script.js
+++ script.js
@@ -55,3 +55,57 @@ function beyond(num) {
         console.log('And beyond');
     }
 }
+
+function decode(word) {
+    let array = word.split(" ");
+    const hiddenMessage = [];
+    // how to break string into array with individual words????
+
+    for (let i = 0; i <= word.length; i++) {
+        const hiddenMessage = word[i];
+        if (word[0] === 'a') {
+            return word[1];
+        }
+        if (word[0] === 'b') {
+            return word[2];
+        }
+        if (word[0] === 'c') {
+            return word[3];
+        }
+        if (word[0] === 'd') {
+            return word[4];
+        }
+        else {
+            return ' ';
+        }
+    }
+    return hiddenMessage;
+}
+
+decode('craft block argon meter bells brown croon droop');
+
+
+function daysInAMonth(month, leapYear) {
+    switch(month) {
+        case 'January':
+        case 'March':
+        case 'May':
+        case 'July':
+        case 'August':
+        case 'October':
+        case 'December':
+            console.log(month + ' has 31 days.');
+            break;
+        case 'April':
+        case 'June':
+        case 'September':
+        case 'November':
+            console.log(month + ' has 30 days.');
+            break;
+        default:
+            throw new Error('Must provide a valid month.');
+            break;
+    }
+    // what to do with february???????????????
+}
+
